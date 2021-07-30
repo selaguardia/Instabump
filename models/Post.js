19ba5797afbeb89b403a57b875bd82fd026dd046
aggarabedian <abrahamgarabedian@gmail.com>
2021-07-30 14:13:54 -0700

    Adds models stub and Post schema.

---
+++ models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  caption: {
+    type: String,
+    required: [true, "You must provide a caption."],
+  },
+  image: {
+    type: String,
+    required: [true, "You must add an image."]
+  },
+  bumpCount: {
+    type: Number,
+    default: 0,
+  },
+  isPinned: {
+    type: Boolean,
+    default: false,
+  }
+},
+  {timestamps: true});
+
+const Post = mongoose.model("Post", postSchema);
+
+
+module.exports = Post;
