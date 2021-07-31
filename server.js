159a663bc310dbeaed25b7806e2178cc693450c7
aggarabedian <abrahamgarabedian@gmail.com>
2021-07-30 22:03:57 -0700

    Basic express routes

--- server.js
+++ server.js
@@ -1,42 +1,25 @@
-/* SECTION External modules */
+// External Modules
 const express = require("express");
-const methodOverride = require("method-override");
 
-/* SECTION module instance */
+// Create the express app
 const app = express();
 
-/* SECTION PORT  */
+
+// PORT
 const PORT = 4000;
 
-/* SECTION Internal Modules */
-const controllers = require("./controllers");
 
-/* SECTION App Config */
-
-app.set("view engine", "ejs");
-
-/* SECTION Middleware */
-
-app.use(express.static("public"));
-
-// NOTE allow body data for all routes
-app.use(express.urlencoded({ extended: true }));
-
-app.use(methodOverride("_method"));
-
-/* !SECTION */
-
-/* SECTION Routes */
-
-// 404
-app.get("/*", (req, res) => {
-  const context = {
-    error: req.error,
-  };
-
-  res.render("404", context);
+// Routes
+app.get("/", (req, res) => {
+  res.redirect("/home");
 });
 
-app.listen(PORT, () =>
-  console.log(`Listening for client requests on port ${PORT}`)
-);
+app.get("/home", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+
+// Tell the app to listen on port 4000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
