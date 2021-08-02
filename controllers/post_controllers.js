236c050b453209e938e6d96d4acdef957770fd09
aggarabedian <abrahamgarabedian@gmail.com>
2021-08-02 10:23:12 -0700

    Added post index route.

--- controllers/post_controllers.js
+++ controllers/post_controllers.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-const { Post, User } = require("../models");
+const { Post } = require("../models");
 
 // Index route
 router.get("/", (req, res) => {
