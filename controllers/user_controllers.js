f123c7aaa024a3c4a26df4f15552c34efc42eff9
aggarabedian <abrahamgarabedian@gmail.com>
2021-08-02 13:36:36 -0700

    Added index route to user controllers.

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 const { User } = require("../models");
 
+// Index Route
+router.get("/", (req, res) => {
+  User.find({}, (error, allUsers) => {
+    if (error) {
+      console.log(error);
+      req.error = error;
+      return next();
+    }
 
+    const context = {
+      users: allUsers
+    };
+    return res.render("users/index", context);
+  });
+});
 
 
 
