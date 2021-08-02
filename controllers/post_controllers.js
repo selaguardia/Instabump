779eb20d3a37993401dd7317c73d7097c47eec9f
aggarabedian <abrahamgarabedian@gmail.com>
2021-08-02 10:27:25 -0700

    Added new form and create post routes

--- controllers/post_controllers.js
+++ controllers/post_controllers.js
@@ -4,7 +4,39 @@ const { Post } = require("../models");
 
 // Index route
 router.get("/", (req, res) => {
-  res.send("It works!");
+  Post.find({}, (error, allPosts) => {
+    if (error) {
+      console.log(error);
+      req.error = error;
+      return next();
+    }
+
+    const context = {
+      posts: allPosts,
+    };
+
+    res.render("index", context);
+  });
+});
+
+// New Post Form Route
+router.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+// Create New Post Route
+router.post("/", (req, res, next) => {
+  Post.create(req.body, (error, createdPost) => {
+    if (error) {
+      console.log(error);
+      req.error = error;
+
+      const context = {error};
+
+      return res.render("new", context);
+    };
+    return res.redirect(`/posts`);
+  });
 });
 
 module.exports = router;
