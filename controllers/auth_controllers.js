6421da394b29259d21b768a3b6c6f017f5e6547b
aggarabedian <abrahamgarabedian@gmail.com>
2021-08-02 16:27:13 -0700

    Added auth register post route, set up bcrypt, express-session, MongoStore

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { User } = require("../models");
 
 // Register Get Route
@@ -12,5 +13,27 @@ router.get("/login", (req, res) => {
   return res.render("auth/login");
 });
 
+// Register Post Route
+router.post("/register", async (req, res) => {
+  try {
+    const foundUser = await User.exists({email: req.body.email});
+    if (foundUser) {
+      return res.redirect("/login");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hash = await bcrypt.hash(req.body.password, salt);
+
+    req.body.password = hash;
+
+    const newUser = await User.create(req.body);
+
+    return res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+    return res.send(error);
+  }
+});
 
 module.exports = router;
