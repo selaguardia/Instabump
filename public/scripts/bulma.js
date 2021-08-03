5620b069c2f1ab9d804f996bbbf948e3eb21f5e9
selaguardia <selaguardia91@gmail.com>
2021-08-02 17:19:51 -0700

    ejs routes added, but need to be fixed

---
+++ public/scripts/bulma.js
@@ -0,0 +1,10 @@
+$(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+  
+  $(".toggle-modal").on("click", e => {
+    $(".modal").toggleClass("is-active");
+  });
+  
